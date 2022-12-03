607d5b44854c81f7426c349d2ddd552acc05923b
WDMdanila <gendalfiori@gmail.com>
2022-12-03 07:50:30 +0100

    Little more sense

--- include/CommunicationFactory.hpp
+++ include/CommunicationFactory.hpp
@@ -18,12 +18,11 @@ public:
 
     template <typename G, typename S, typename R>
     std::pair<std::unique_ptr<ProviderSwarm<T>>, std::unique_ptr<Requester<T>>> create_swarm(uint num,
-        G&& generator,
-        S&& sender,
+        G generator,
+        S sender,
         R&& requester) {
         static_assert(std::is_copy_constructible_v<G>, "Generator must be copy constructible");
         static_assert(std::is_copy_constructible_v<S>, "Sender must be copy constructible");
-        static_assert(std::is_copy_constructible_v<R>, "Requester must be copy constructible");
         std::vector<std::unique_ptr<Provider<T>>> providers {};
         std::vector<std::shared_ptr<oneapi::tbb::concurrent_queue<T>>> queues {};
 
@@ -33,8 +32,8 @@ public:
             providers.emplace_back(
                 std::make_unique<Provider<T>>(
                     std::move(queue),
-                    std::forward<G>(generator),
-                    std::forward<S>(sender)));
+                    generator,
+                    sender));
         }
         return {std::make_unique<ProviderSwarm<T>>(std::move(providers)),
                 std::make_unique<Requester<T>>(queues, std::forward<R>(requester))};
